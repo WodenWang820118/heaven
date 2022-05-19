609c7e18ae29d86c489d77e770a1ea3ad18cc10e
Guan-Xin <alex0972112871@gmail.com>
2022-05-19 20:19:07 +0800

    serviceRandom ready; include seed in properties file

---
+++ src/oh_heaven/game/service/ServiceRandom.java
@@ -0,0 +1,27 @@
+package oh_heaven.game.service;
+
+import java.util.Random;
+
+public class ServiceRandom {
+    private static Random random;
+
+    private ServiceRandom() {}
+
+    // class requires explicit initialisation and assumes usage of random is serialised
+    public static void initServicesRandom(Long seed) {
+        if (random == null) {
+            if (seed == null) {
+                random = new Random();
+                System.out.println("Seed = null");
+            } else { // Use specified seed
+                random = new Random(seed);
+                System.out.println("Seed = " + seed);
+            }
+        }
+    }
+
+    public static Random getSeedRandom() {
+        return random;
+    }
+}
+
