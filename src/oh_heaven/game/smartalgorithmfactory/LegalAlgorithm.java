e6c34df7833e63c19d4b3d6ddb39930f66a72eed
RuotongZhao <ruotzhao@student.unimelb.edu.au>
2022-05-24 16:16:01 +0800

    legalAlgorithm

--- src/oh_heaven/game/smartalgorithmfactory/LegalAlgorithm.java
+++ src/oh_heaven/game/smartalgorithmfactory/LegalAlgorithm.java
@@ -1,11 +1,21 @@
 package oh_heaven.game.smartalgorithmfactory;
 
 import ch.aplu.jcardgame.Card;
+import ch.aplu.jcardgame.Hand;
 import oh_heaven.game.playerboard.player.Player;
+import oh_heaven.game.service.Rule;
+import oh_heaven.game.utilities.ServiceRandom;
 
 public class LegalAlgorithm implements IAlgorithm {
+    Rule rule;
     @Override
     public Card nextPlay(Player player) {
-        return null;
+        rule.setEnforceRules(true);
+        Hand hand = player.getDeck();
+        return randomCard(hand);
+    }
+    public Card randomCard(Hand hand){
+        int x = ServiceRandom.getSeedRandom().nextInt(hand.getNumberOfCards());
+        return hand.get(x);
     }
 }
